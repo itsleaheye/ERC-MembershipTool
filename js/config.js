876cfb11118f520e3459536406831ab0cb3fe9d6
leahnonay <61296534+leahnonay@users.noreply.github.com>
2021-12-24 12:59:37 -0700

    Polish
    - Update Banner min sizes on larger screens and positioning
    - Remove unused stylesheet that I used as refrence from prior projects
    - Started JS for loading the member ID

---
+++ js/config.js
@@ -0,0 +1,28 @@
+// import { initializeApp } from 'firebase/app';
+// import { getDatabase } from "firebase/database";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// <script src="https://www.gstatic.com/firebasejs/5.0.1/firebase-app.js"></script>
+
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const config = {
+//   apiKey: "AIzaSyCUkrg8V2jEiEJLJXDc13fw-xq91ycOgLw",
+//   authDomain: "edmontonrocketryclub.firebaseapp.com",
+//   databaseURL: "https://edmontonrocketryclub-default-rtdb.firebaseio.com",
+//   projectId: "edmontonrocketryclub",
+//   storageBucket: "edmontonrocketryclub.appspot.com",
+//   messagingSenderId: "809231412301",
+//   appId: "1:809231412301:web:74abb7eb22666dcc220004",
+//   measurementId: "G-SHCVKN59R6"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(config);
+
+// // Get a reference to the database service
+// const database = getDatabase(app);
